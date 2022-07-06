d079642311e787d00678d632d96caeda71eeddcc
andredcastro43 <andreccastro@icloud.com>
2022-07-05 21:14:25 -0300

    exercicio concluido

---
+++ main.py
@@ -0,0 +1,43 @@
+#Programa em Python que solicite as informações de três pessoas, como nome, idade, peso, altura. Apresenta na tela estas informações de modo que permaneçam alinhados verticalmente com formatação de interpolação.
+
+idade = 20 
+
+input('preencha seu nome, usuario 1:')
+print('Que nome bonito!')
+
+input('Agora me diga sua idade: ')
+if idade > 20:
+print('Voce esta velho...')
+if idade < 20"
+print ('voce ainda esta novo')
+
+input('Pode me dizer qual seu peso?')
+print('Ta precisando treinar.')
+
+input('Me diga sua altura: ')
+print('Que alto! Agora vamos para o usuario 2')
+
+input('preencha seu nome, usuario 2:')
+print('Nao e tao bonito!')
+
+input('Agora me diga sua idade: ')
+print('Otimo...')
+
+input('Pode me dizer qual seu peso?')
+print('Ta comendo muito.')
+
+input('Me diga sua altura: ')
+print('Que alto! Agora vamos para o usuario 3')
+
+input('Usuario 3, me diga seu nome:')
+print('Eu prefiro Andre!')
+
+input('Agora me diga sua idade: ')
+print('Ta com cabelo branco...')
+
+input('Pode me dizer qual seu peso?')
+print('Legal.')
+
+input('Me diga sua altura: ')
+print('Terminamos nossa pesquisa.')
+
